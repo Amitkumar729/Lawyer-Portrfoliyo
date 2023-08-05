8a019bf99382db6fa0200ecbc3a2e3793f7e6fcf
Amit kumar <ak0638010@gmail.com>
2023-08-05 12:52:26 +0530

    final commit

--- src/constant/practiceArea/index.js
+++ src/constant/practiceArea/index.js
@@ -36,6 +36,9 @@ export const Practices = [
     title: "RERA",
   },
   {
+    title: "Trade Marks & IPR",
+  },
+  {
     title: "Consumer Forum",
   },
   {
--- src/styles/view/blog/index.jsx
+++ src/styles/view/blog/index.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 width: 100%;
-height: 100%;
+/* height: 100%; */
 /* border: 1px solid grey; */
 display: flex;
 align-items: center;
